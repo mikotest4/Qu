2368adefef73141cbed3166e9b963b53d3d86605
VkTheEncoder <158612790+VkTheEncoder@users.noreply.github.com>
2025-04-24 19:16:56 +0530

    Update muxbot.py

--- muxbot.py
+++ muxbot.py
@@ -1,36 +1,30 @@
-# (c) mohdsabahat
-
-import logging
-logging.basicConfig(
-    level=logging.DEBUG,
-    format="%(asctime)s - %(name)s - %(message)s - %(levelname)s"
-)
-import os
+import logging, os
 from config import Config
 from helper_func.dbhelper import Database as Db
-db = Db().setup()
+from plugins.muxer import queue_worker
 
-import pyrogram
+logging.basicConfig(level=logging.DEBUG,
+                    format="%(asctime)s - %(name)s - %(message)s")
 logging.getLogger('pyrogram').setLevel(logging.WARNING)
 
-from plugins.muxer import queue_worker  # ensure this import
+db = Db().setup()
+if not os.path.isdir(Config.DOWNLOAD_DIR):
+    os.mkdir(Config.DOWNLOAD_DIR)
 
-class QueueBot(pyrogram.Client):
+from pyrogram import Client
+class QueueBot(Client):
     async def start(self):
         await super().start()
-        # launch the queue processor
+        # launch our single background worker
         self.loop.create_task(queue_worker(self))
 
-if __name__ == '__main__':
-    if not os.path.isdir(Config.DOWNLOAD_DIR):
-        os.mkdir(Config.DOWNLOAD_DIR)
+app = QueueBot(
+    "SubtitleMuxer",
+    bot_token=Config.BOT_TOKEN,
+    api_id=Config.APP_ID,
+    api_hash=Config.API_HASH,
+    plugins=dict(root="plugins")
+)
 
-    plugins = dict(root='plugins')
-    app = QueueBot(
-        'Subtitle Muxer',
-        bot_token=Config.BOT_TOKEN,
-        api_id=Config.APP_ID,
-        api_hash=Config.API_HASH,
-        plugins=plugins
-    )
+if __name__ == "__main__":
     app.run()
