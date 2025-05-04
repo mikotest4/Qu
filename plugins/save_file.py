01966b803e1120d9b9d2ab8690f838a5f89c9ed3
VkTheEncoder <158612790+VkTheEncoder@users.noreply.github.com>
2025-05-04 09:41:25 +0530

    Update save_file.py

--- plugins/save_file.py
+++ plugins/save_file.py
@@ -20,24 +20,21 @@ db = Db()
 
 async def _check_user(filt, c, m):
     chat_id = str(m.from_user.id)
-    if chat_id in Config.ALLOWED_USERS:
-        return True
-    else :
-        return False
+    return chat_id in Config.ALLOWED_USERS
 
 check_user = filters.create(_check_user)
 
+
 @Client.on_message(filters.document & check_user & filters.private)
 async def save_doc(client, message):
-
-    chat_id = message.from_user.id
+    chat_id    = message.from_user.id
     start_time = time.time()
     downloading = await client.send_message(chat_id, 'Downloading your File!')
     download_location = await client.download_media(
-        message = message,
-        file_name = Config.DOWNLOAD_DIR+'/',
-        progress = progress_bar,
-        progress_args = (
+        message=message,
+        file_name=Config.DOWNLOAD_DIR + '/',
+        progress=progress_bar,
+        progress_args=(
             'Initializing',
             downloading,
             start_time
@@ -46,15 +43,15 @@ async def save_doc(client, message):
 
     if download_location is None:
         return client.edit_message_text(
-            text = 'Downloading Failed!',
-            chat_id = chat_id,
-            message_id = downloading.id
+            text='Downloading Failed!',
+            chat_id=chat_id,
+            message_id=downloading.id
         )
 
     await client.edit_message_text(
-        text = Chat.DOWNLOAD_SUCCESS.format(round(time.time()-start_time)),
-        chat_id = chat_id,
-        message_id = downloading.id
+        text=Chat.DOWNLOAD_SUCCESS.format(round(time.time() - start_time)),
+        chat_id=chat_id,
+        message_id=downloading.id
     )
 
     tg_filename = os.path.basename(download_location)
@@ -63,190 +60,199 @@ async def save_doc(client, message):
     except:
         og_filename = False
 
-    if og_filename:
-        #os.rename(Config.DOWNLOAD_DIR+'/'+tg_filename,Config.DOWNLOAD_DIR+'/'+og_filename)
-        save_filename = og_filename
-    else :
-        save_filename = tg_filename
-
+    save_filename = og_filename or tg_filename
     ext = save_filename.split('.').pop()
-    filename = str(round(start_time))+'.'+ext
+    filename = f"{round(start_time)}.{ext}"
 
-    if ext in ['srt','ass']:
-        os.rename(Config.DOWNLOAD_DIR+'/'+tg_filename,Config.DOWNLOAD_DIR+'/'+filename)
+    os.rename(
+        os.path.join(Config.DOWNLOAD_DIR, tg_filename),
+        os.path.join(Config.DOWNLOAD_DIR, filename)
+    )
+
+    if ext in ['srt', 'ass']:
         db.put_sub(chat_id, filename)
         if db.check_video(chat_id):
-            text = 'Subtitle file downloaded successfully.\nChoose your desired muxing!\n[ /softmux , /hardmux ]'
+            text = (
+                'Subtitle file downloaded successfully.\n'
+                'Choose your desired muxing!\n'
+                '[ /softmux , /hardmux ]'
+            )
         else:
             text = 'Subtitle file downloaded.\nNow send Video File!'
 
         await client.edit_message_text(
-            text = text,
-            chat_id = chat_id,
-            message_id = downloading.id
+            text=text,
+            chat_id=chat_id,
+            message_id=downloading.id
         )
 
-    elif ext in ['mp4','mkv']:
-        os.rename(Config.DOWNLOAD_DIR+'/'+tg_filename,Config.DOWNLOAD_DIR+'/'+filename)
+    elif ext in ['mp4', 'mkv']:
         db.put_video(chat_id, filename, save_filename)
         if db.check_sub(chat_id):
-            text = 'Video file downloaded successfully.\nChoose your desired muxing.\n[ /softmux , /hardmux ]'
-        else :
-            text = 'Video file downloaded successfully.\nNow send Subtitle file!'
+            text = (
+                'Video file downloaded successfully.\n'
+                'Choose your desired muxing.\n'
+                '[ /softmux , /hardmux ]'
+            )
+        else:
+            text = (
+                'Video file downloaded successfully.\n'
+                'Now send Subtitle file!\n'
+                'Or send /nosub to encode without subtitles.'
+            )
+
         await client.edit_message_text(
-            text = text,
-            chat_id = chat_id,
-            message_id = downloading.id
+            text=text,
+            chat_id=chat_id,
+            message_id=downloading.id
         )
 
     else:
-        text = Chat.UNSUPPORTED_FORMAT.format(ext)+f'\nFile = {tg_filename}'
+        text = Chat.UNSUPPORTED_FORMAT.format(ext) + f'\nFile = {tg_filename}'
         await client.edit_message_text(
-            text = text,
-            chat_id = chat_id,
-            message_id = downloading.id
+            text=text,
+            chat_id=chat_id,
+            message_id=downloading.id
         )
-        os.remove(Config.DOWNLOAD_DIR+'/'+tg_filename)
+        os.remove(os.path.join(Config.DOWNLOAD_DIR, tg_filename))
 
 
 @Client.on_message(filters.video & check_user & filters.private)
 async def save_video(client, message):
-
-    chat_id = message.from_user.id
+    chat_id    = message.from_user.id
     start_time = time.time()
     downloading = await client.send_message(chat_id, 'Downloading your File!')
     download_location = await client.download_media(
-        message = message,
-        file_name = Config.DOWNLOAD_DIR+'/',
-        progress = progress_bar,
-        progress_args = (
+        message=message,
+        file_name=Config.DOWNLOAD_DIR + '/',
+        progress=progress_bar,
+        progress_args=(
             'Initializing',
             downloading,
             start_time
-            )
         )
+    )
 
     if download_location is None:
         return client.edit_message_text(
-            text = 'Downloading Failed!',
-            chat_id = chat_id,
-            message_id = downloading.id
+            text='Downloading Failed!',
+            chat_id=chat_id,
+            message_id=downloading.id
         )
 
     await client.edit_message_text(
-        text = Chat.DOWNLOAD_SUCCESS.format(round(time.time()-start_time)),
-        chat_id = chat_id,
-        message_id = downloading.id
+        text=Chat.DOWNLOAD_SUCCESS.format(round(time.time() - start_time)),
+        chat_id=chat_id,
+        message_id=downloading.id
     )
 
     tg_filename = os.path.basename(download_location)
     try:
-        og_filename = message.document.filename
+        og_filename = message.video.file_name
     except:
         og_filename = False
-    
-    if og_filename:
-        save_filename = og_filename
-    else :
-        save_filename = tg_filename
-    
+
+    save_filename = og_filename or tg_filename
     ext = save_filename.split('.').pop()
-    filename = str(round(start_time))+'.'+ext
-    os.rename(Config.DOWNLOAD_DIR+'/'+tg_filename,Config.DOWNLOAD_DIR+'/'+filename)
-    
+    filename = f"{round(start_time)}.{ext}"
+    os.rename(
+        os.path.join(Config.DOWNLOAD_DIR, tg_filename),
+        os.path.join(Config.DOWNLOAD_DIR, filename)
+    )
+
     db.put_video(chat_id, filename, save_filename)
     if db.check_sub(chat_id):
-        text = 'Video file downloaded successfully.\nChoose your desired muxing.\n[ /softmux , /hardmux ]'
-    else :
-        text = 'Video file downloaded successfully.\nNow send Subtitle file!'
+        text = (
+            'Video file downloaded successfully.\n'
+            'Choose your desired muxing.\n'
+            '[ /softmux , /hardmux ]'
+        )
+    else:
+        text = (
+            'Video file downloaded successfully.\n'
+            'Now send Subtitle file!\n'
+            'Or send /nosub to encode without subtitles.'
+        )
+
     await client.edit_message_text(
-            text = text,
-            chat_id = chat_id,
-            message_id = downloading.id
-            )
+        text=text,
+        chat_id=chat_id,
+        message_id=downloading.id
+    )
 
 
 @Client.on_message(filters.text & filters.regex('^http') & check_user)
 async def save_url(client, message):
-
-    chat_id = message.from_user.id
+    chat_id      = message.from_user.id
     save_filename = None
 
-    if "|" in message.text:
-        if len(message.text.split('|')) ==2 :
-            save_filename = message.text.split('|')[1].strip()
-            url = message.text.split('|')[0].strip()
-    else :
+    # parse custom filename
+    if "|" in message.text and len(message.text.split("|")) == 2:
+        url, save_filename = [x.strip() for x in message.text.split("|")]
+    else:
         url = message.text.strip()
 
-    if save_filename and len(save_filename)>60:
-        return await client.sendMessage(chat_id, Chat.LONG_CUS_FILENAME)
+    # enforce custom filename length
+    if save_filename and len(save_filename) > 60:
+        return await client.send_message(chat_id, Chat.LONG_CUS_FILENAME)
 
+    # HEAD request to find filename & size
     r = requests.get(url, stream=True, allow_redirects=True)
-    if save_filename is None :
-        if 'content-disposition' in r.headers.keys() :
-            regx = 'filename="(.*?)"'
-            res = re.search(regx, str(r.headers))
-            if res :
-                save_filename = res.group(1)
-            else :
-                #removing argumets from url!
-                if '?' in url:
-                    url = ''.join(url.split('?')[0:-1])
-                save_filename = url.split('/')[-1]
-                save_filename = unquote(save_filename)
-        else :
-            if '?' in url:
-                url = ''.join(url.split('?')[0:-1])
-            save_filename = url.split('/')[-1]
-            save_filename = unquote(save_filename)
+    if not save_filename:
+        # try content-disposition
+        cd = r.headers.get("content-disposition", "")
+        m  = re.search(r'filename="(.+?)"', cd)
+        if m:
+            save_filename = m.group(1)
+        else:
+            save_filename = unquote(url.split("?")[0].split("/")[-1])
 
+    ext = save_filename.split(".")[-1].lower()
+    if ext not in ["mp4", "mkv"]:
+        return await client.send_message(chat_id, Chat.UNSUPPORTED_FORMAT.format(ext))
+
+    size = int(r.headers.get("content-length", 0))
+    if size == 0:
+        return await client.send_message(chat_id, Chat.FILE_SIZE_ERROR)
+    if size > 2_000_000_000:
+        return await client.send_message(chat_id, Chat.MAX_FILE_SIZE)
+
+    # download to disk
+    if not os.path.exists(Config.DOWNLOAD_DIR):
+        os.makedirs(Config.DOWNLOAD_DIR)
+
+    filename = f"{round(time.time())}.{ext}"
     sent_msg = await client.send_message(chat_id, 'Preparing Your Download')
-    ext = save_filename.split('.')[-1]
-    if ext not in ['mp4','mkv'] :
-        return await sent_msg.edit(Chat.UNSUPPORTED_FORMAT.format(ext))
-
-    size = None
-    if 'content-length' in r.headers.keys() :
-        size = int(r.headers['content-length'])
-
-    if not size :
-        return await sent_msg.edit(Chat.FILE_SIZE_ERROR)
-    if size>(2*1000*1000*1000) :
-        return await sent_msg.edit(Chat.MAX_FILE_SIZE)
-
-    if not os.path.exists(Config.DOWNLOAD_DIR) :
-        os.mkdir(Config.DOWNLOAD_DIR)
 
     current = 0
-    start = time.time()
-    filename = str(round(start))+'.'+ext
-
-    logging.info(url)
-
-    with requests.get(url, stream=True, allow_redirects=True) as r :
-        with open(os.path.join(Config.DOWNLOAD_DIR,filename), 'wb') as f :
-            for chunk in r.iter_content(chunk_size=1024*1024) :
-                if (chunk) :
+    start   = time.time()
+    with requests.get(url, stream=True, allow_redirects=True) as resp:
+        with open(os.path.join(Config.DOWNLOAD_DIR, filename), "wb") as f:
+            for chunk in resp.iter_content(chunk_size=1024*1024):
+                if chunk:
                     written = f.write(chunk)
-                    #current += 1024*1024
                     current += written
-                    await progress_bar(current, size, 'Downloading Your File!', sent_msg, start)
-
-    logging.info(save_filename)
+                    await progress_bar(current, size, 'Downloading…', sent_msg, start)
 
     try:
-        await sent_msg.edit(
-            Chat.DOWNLOAD_SUCCESS.format(round(time.time()-start))
-            )
+        await sent_msg.edit(Chat.DOWNLOAD_SUCCESS.format(round(time.time() - start)))
     except:
         pass
 
     db.put_video(chat_id, filename, save_filename)
-    if db.check_sub(chat_id) :
-        text = 'Video File Downloaded.\nChoose your desired muxing\n[ /softmux , /hardmux ]'
-    else :
-        text = 'Video File Downloaded.\nNow send Subtitle file!'
+    if db.check_sub(chat_id):
+        text = (
+            'Video File Downloaded.\n'
+            'Choose your desired muxing\n'
+            '[ /softmux , /hardmux ]'
+        )
+    else:
+        text = (
+            'Video File Downloaded.\n'
+            'Now send Subtitle file!\n'
+            'Or send /nosub to encode without subtitles.'
+        )
+
     try:
         await sent_msg.edit(text)
     except:
