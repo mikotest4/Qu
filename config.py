68d58870a9db9f8dad4d9e7b5a448ceb4493043c
mikotest4 <fakealok756@gmail.com>
2025-07-03 20:35:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ class Config:
     API_HASH = "c2e33826d757fe113bc154fcfabc987d"
 
     #comma seperated user id of users who are allowed to use
-    ALLOWED_USERS = [x.strip(' ') for x in os.environ.get('ALLOWED_USERS','7970350353').split(',')]
+    ALLOWED_USERS = [x.strip(' ') for x in os.environ.get('ALLOWED_USERS','7970350353, 6680407688').split(',')]
  # Absolute path to the folder where you keep your .ttf/.otf files
     FONTS_DIR = os.path.join(os.getcwd(), "fonts")
     
