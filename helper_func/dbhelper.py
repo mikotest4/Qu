35ac8c56569298ee1afe1af3a4fc13681cac995b
mikotest4 <fakealok756@gmail.com>
2025-07-16 19:34:46 +0530

    Update dbhelper.py

--- helper_func/dbhelper.py
+++ helper_func/dbhelper.py
@@ -3,113 +3,131 @@ import sqlite3
 class Database:
 
     def __init__(self):
-
         self.conn = sqlite3.connect('muxdb.sqlite', check_same_thread = False)
 
     def setup(self):
-
         cmd = """CREATE TABLE IF NOT EXISTS muxbot(
         user_id INT,
         vid_name TEXT,
         sub_name TEXT,
+        font_name TEXT,
         filename TEXT
         );"""
-
         self.conn.execute(cmd)
         self.conn.commit()
 
     def put_video(self, user_id, vid_name, filename):
-
-        ins_cmd = 'INSERT INTO muxbot VALUES (?,?,?,?);'
+        ins_cmd = 'INSERT INTO muxbot VALUES (?,?,?,?,?);'
         srch_cmd = f'SELECT * FROM muxbot WHERE user_id={user_id};'
         up_cmd = f'UPDATE muxbot SET vid_name="{vid_name}", filename="{filename}" WHERE user_id={user_id};'
-        data = (user_id, vid_name, None, filename)
+        data = (user_id, vid_name, None, None, filename)
         res = self.conn.execute(srch_cmd).fetchone()
         if res:
             self.conn.execute(up_cmd)
             self.conn.commit()
-        else :
-            self.conn.execute(ins_cmd,data)
+        else:
+            self.conn.execute(ins_cmd, data)
             self.conn.commit()
 
-    def put_sub(self,user_id,sub_name) :
-
-        ins_cmd = 'INSERT INTO muxbot VALUES (?,?,?,?);'
+    def put_sub(self, user_id, sub_name):
+        ins_cmd = 'INSERT INTO muxbot VALUES (?,?,?,?,?);'
         srch_cmd = f'SELECT * FROM muxbot WHERE user_id={user_id};'
         up_cmd = f'UPDATE muxbot SET sub_name="{sub_name}" WHERE user_id={user_id};'
-        data = (user_id,None,sub_name,None)
-
+        data = (user_id, None, sub_name, None, None)
         res = self.conn.execute(srch_cmd).fetchone()
-        if res :
+        if res:
             self.conn.execute(up_cmd)
             self.conn.commit()
-        else :
-            self.conn.execute(ins_cmd,data)
+        else:
+            self.conn.execute(ins_cmd, data)
             self.conn.commit()
 
-    def check_sub(self,user_id) :
-
+    def put_font(self, user_id, font_name):
+        ins_cmd = 'INSERT INTO muxbot VALUES (?,?,?,?,?);'
         srch_cmd = f'SELECT * FROM muxbot WHERE user_id={user_id};'
-
+        up_cmd = f'UPDATE muxbot SET font_name="{font_name}" WHERE user_id={user_id};'
+        data = (user_id, None, None, font_name, None)
         res = self.conn.execute(srch_cmd).fetchone()
-        if res :
+        if res:
+            self.conn.execute(up_cmd)
+            self.conn.commit()
+        else:
+            self.conn.execute(ins_cmd, data)
+            self.conn.commit()
 
+    def check_sub(self, user_id):
+        srch_cmd = f'SELECT * FROM muxbot WHERE user_id={user_id};'
+        res = self.conn.execute(srch_cmd).fetchone()
+        if res:
             sub_file = res[2]
-            if sub_file :
+            if sub_file:
                 return True
-            else :
+            else:
                 return False
-
-        else :
+        else:
             return False
 
-    def check_video(self,user_id) :
-
+    def check_video(self, user_id):
         srch_cmd = f'SELECT * FROM muxbot WHERE user_id={user_id};'
         res = self.conn.execute(srch_cmd).fetchone()
-        if res :
+        if res:
             vid_file = res[1]
-            if vid_file :
+            if vid_file:
                 return True
-            else :
+            else:
                 return False
-        else :
+        else:
             return False
 
-    def get_vid_filename(self, user_id) :
+    def check_font(self, user_id):
+        srch_cmd = f'SELECT * FROM muxbot WHERE user_id={user_id};'
+        res = self.conn.execute(srch_cmd).fetchone()
+        if res:
+            font_file = res[3]
+            if font_file:
+                return True
+            else:
+                return False
+        else:
+            return False
 
+    def get_vid_filename(self, user_id):
         cmd = f'SELECT * FROM muxbot WHERE user_id={user_id};'
         res = self.conn.execute(cmd).fetchone()
-        if res :
+        if res:
             return res[1]
-        else :
+        else:
             return False
 
-    def get_sub_filename(self, user_id) :
-
+    def get_sub_filename(self, user_id):
         cmd = f'SELECT * FROM muxbot WHERE user_id={user_id};'
         res = self.conn.execute(cmd).fetchone()
-        if res :
+        if res:
             return res[2]
-        else :
+        else:
             return False
 
-    def get_filename(self, user_id) :
-
+    def get_font_filename(self, user_id):
         cmd = f'SELECT * FROM muxbot WHERE user_id={user_id};'
         res = self.conn.execute(cmd).fetchone()
-        if res :
+        if res:
             return res[3]
-        else :
+        else:
             return False
 
-    def erase(self,user_id) :
+    def get_filename(self, user_id):
+        cmd = f'SELECT * FROM muxbot WHERE user_id={user_id};'
+        res = self.conn.execute(cmd).fetchone()
+        if res:
+            return res[4]
+        else:
+            return False
 
-        erase_cmd = f'DELETE FROM muxbot WHERE user_id={user_id} ;'
-
-        try :
+    def erase(self, user_id):
+        erase_cmd = f'DELETE FROM muxbot WHERE user_id={user_id};'
+        try:
             self.conn.execute(erase_cmd)
             self.conn.commit()
             return True
-        except :
+        except:
             return False
